60ffe3b0445f361c28b227d4aafd95cfc8231cfc
fsnajafali <fsnajafali@gmail.com>
2020-09-28 09:25:16 -0400

    Re-arranged the layout

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -3,10 +3,13 @@ package com.codepath.bestsellerlistapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
 import java.util.List;
@@ -48,6 +51,13 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
                 }
             }
         });
+
+        BestSellerBook bestSellerBook = books.get(position);
+
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
     }
 
     @Override
@@ -59,6 +69,10 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         public final View mView;
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
+        public final ImageView mBookImage;
+        public final TextView mRanking;
+        public final TextView mBookDescription;
+        public final Button mBuyButton;
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
@@ -66,11 +80,15 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
             mView = view;
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookImage = (ImageView) view.findViewById(R.id.book_image);
+            mRanking = (TextView) view.findViewById(R.id.ranking);
+            mBookDescription = (TextView) view.findViewById(R.id.book_description);
+            mBuyButton = (Button) view.findViewById(R.id.buy_button);
         }
 
         @Override
         public String toString() {
-            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'";
+            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'" + mBookDescription.getText();
         }
     }
 }
